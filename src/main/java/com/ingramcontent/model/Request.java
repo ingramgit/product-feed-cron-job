e1ef1b92decd6cd2c5f7ecee1bbd96ff6d408fd8
admin <>
2023-12-15 06:52:21 +0000

    {/src/main/java/com/ingramcontent/model/Request.java}

--- src/main/java/com/ingramcontent/model/Request.java
+++ src/main/java/com/ingramcontent/model/Request.java
@@ -6,12 +6,33 @@ package com.ingramcontent.model;
 
 public class Request implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Request() {
-    }
+	private java.lang.String method;
+	private java.lang.String response;
 
+	public Request() {
+	}
 
+	public java.lang.String getMethod() {
+		return this.method;
+	}
 
+	public void setMethod(java.lang.String method) {
+		this.method = method;
+	}
+
+	public java.lang.String getResponse() {
+		return this.response;
+	}
+
+	public void setResponse(java.lang.String response) {
+		this.response = response;
+	}
+
+	public Request(java.lang.String method, java.lang.String response) {
+		this.method = method;
+		this.response = response;
+	}
 
 }
