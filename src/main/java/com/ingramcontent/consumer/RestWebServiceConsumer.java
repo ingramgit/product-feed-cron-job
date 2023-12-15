bd86c8369a7c046cd21a2c4349907fd92b42d03f
admin <>
2023-12-15 07:05:59 +0000

    {/src/main/java/com/ingramcontent/consumer/RestWebServiceConsumer.java}

--- src/main/java/com/ingramcontent/consumer/RestWebServiceConsumer.java
+++ src/main/java/com/ingramcontent/consumer/RestWebServiceConsumer.java
@@ -20,27 +20,29 @@ public class RestWebServiceConsumer implements java.io.Serializable {
     }
 
 
-    public static void get(String uri) throws Exception {
-	    HttpClient client = HttpClient.newHttpClient();
-	    HttpRequest request = HttpRequest.newBuilder()
-	          .uri(URI.create(uri))
-	          .build();
-
-	    HttpResponse<String> response =
-	          client.send(request, BodyHandlers.ofString());
-        
-	    System.out.println("Status Code = " + response.statusCode() + ", Response = "+ response.body());
+    public static String get(String uri) throws Exception {
+    	    HttpClient client = HttpClient.newHttpClient();
+    	    HttpRequest request = HttpRequest.newBuilder()
+    	          .uri(URI.create(uri))
+    	          .build();
+    
+    	    HttpResponse<String> response =
+    	          client.send(request, BodyHandlers.ofString());
+            
+    	    logger.info("Status Code = " + response.statusCode() + ", Response = "+ response.body());
+    	    return response.body();
 	}
 	
-	public static void post(String uri, String data) throws Exception {
-	    HttpClient client = HttpClient.newBuilder().build();
-	    HttpRequest request = HttpRequest.newBuilder()
-	            .uri(URI.create(uri))
-	            .POST(BodyPublishers.ofString(data))
-	            .build();
-
-	    HttpResponse<?> response = client.send(request, BodyHandlers.discarding());
-	    System.out.println("Status Code = " + response.statusCode() + ", Response = "+ response.body());
+	public static Object post(String uri, String data) throws Exception {
+    	    HttpClient client = HttpClient.newBuilder().build();
+    	    HttpRequest request = HttpRequest.newBuilder()
+    	            .uri(URI.create(uri))
+    	            .POST(BodyPublishers.ofString(data))
+    	            .build();
+    
+    	    HttpResponse<?> response = client.send(request, BodyHandlers.discarding());
+    	    logger.info("Status Code = " + response.statusCode() + ", Response = "+ response.body());
+    	    return response.body();
 	}
 
 }
