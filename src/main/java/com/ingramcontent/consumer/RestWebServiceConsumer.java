140f7f4e82be6410fde132b557fc34a064e1afd8
admin <>
2023-12-15 06:55:28 +0000

    {/src/main/java/com/ingramcontent/consumer/RestWebServiceConsumer.java}

--- src/main/java/com/ingramcontent/consumer/RestWebServiceConsumer.java
+++ src/main/java/com/ingramcontent/consumer/RestWebServiceConsumer.java
@@ -12,6 +12,27 @@ public class RestWebServiceConsumer implements java.io.Serializable {
     }
 
 
+    public static void get(String uri) throws Exception {
+	    HttpClient client = HttpClient.newHttpClient();
+	    HttpRequest request = HttpRequest.newBuilder()
+	          .uri(URI.create(uri))
+	          .build();
 
+	    HttpResponse<String> response =
+	          client.send(request, BodyHandlers.ofString());
+        
+	    System.out.println("Status Code = " + response.statusCode() + ", Response = "+ response.body());
+	}
+	
+	public static void post(String uri, String data) throws Exception {
+	    HttpClient client = HttpClient.newBuilder().build();
+	    HttpRequest request = HttpRequest.newBuilder()
+	            .uri(URI.create(uri))
+	            .POST(BodyPublishers.ofString(data))
+	            .build();
+
+	    HttpResponse<?> response = client.send(request, BodyHandlers.discarding());
+	    System.out.println("Status Code = " + response.statusCode() + ", Response = "+ response.body());
+	}
 
 }
